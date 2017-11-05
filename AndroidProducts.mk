bcdb1c5e6f91d9d79969b9385baa313ca40ce824
Ryogo <ryogo.z@gmail.com>
2017-11-05 21:51:00 +0200

    - initial commit;

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/BNRV510.mk \
+    $(LOCAL_DIR)/omni_BNRV510.mk
---
+++ omni_BNRV510.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/NOOK/BNRV510/BNRV510.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := BNRV510
+PRODUCT_NAME := omni_BNRV510
+PRODUCT_BRAND := NOOK
+PRODUCT_MODEL := Glowlight Plus
+PRODUCT_MANUFACTURER := BarnesAndNoble
